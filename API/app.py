5c0b66679535fe7b95c0059548281ba517bfd46a
Kartheeswaran R <kartheeswaran.r@n7banking.com>
2024-09-13 14:50:23 +0530

    run api in docker mode

--- API/app.py
+++ API/app.py
@@ -292,6 +292,7 @@ def delete_employee(name):
 # * -------------------- RUN SERVER -------------------- *
 if __name__ == '__main__':
     # * --- DEBUG MODE: --- *
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    # app.run(host='127.0.0.1', port=5000, debug=True)
     #  * --- DOCKER PRODUCTION MODE: --- *
+    app.run(host='0.0.0.0', port=5000)
     # app.run(host='0.0.0.0', port=os.environ['PORT']) -> DOCKER
