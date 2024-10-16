68b86f64e43f6c9bf778b3b0173c3aeeabfbd6b8
Kartheeswaran R <kartheeswaran.r@n7banking.com>
2024-10-17 00:13:55 +0530

    configured websocket url with https

--- Mood_Board_Frontend/src/pages/module/admin/dashboard/socket.jsx
+++ Mood_Board_Frontend/src/pages/module/admin/dashboard/socket.jsx
@@ -1,7 +1,8 @@
 import io from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://20.204.226.52:9002/';
+const URL = 'https://okotech.ai/ws';
 export const socket = io(URL, {
+    path: '/ws/socket.io',
     transports: ['websocket']
  });
