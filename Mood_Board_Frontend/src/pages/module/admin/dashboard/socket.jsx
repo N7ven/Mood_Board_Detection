be7ac5ae5e10b4c6328d8c04244a77037b08e5fb
Kartheeswaran R <kartheeswaran.r@n7banking.com>
2024-10-16 13:47:23 +0530

    added dependency versions

--- Mood_Board_Frontend/src/pages/module/admin/dashboard/socket.jsx
+++ Mood_Board_Frontend/src/pages/module/admin/dashboard/socket.jsx
@@ -2,4 +2,6 @@ import io from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = 'http://20.204.226.52:9002/';
-export const socket = io(URL);
+export const socket = io(URL, {
+    transports: ['websocket']
+ });
